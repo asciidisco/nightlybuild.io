f1ba4994919ab613d06634da15396fe4f67800a5
Sebastian Golasch <public@asciidisco.com>
2014-07-17 14:15:51 +0200

    Make the @dublic happy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,17 @@
 module.exports = function(grunt) {
+  
   grunt.initConfig({
+    
     clean: {
+      // clean `distributable` folder (most probably before triggering a new build)
       dist: ['dist'],
+      // clean non-used artifacts from the `distributable` folder after the build
       afterMin: ['dist/style.css', 'dist/main.js']
     },
+    
     copy: {
+      // copy over single files from the root directory to the `distributable` folder
+      // add assets if needed
       main: {
         files: [
           {expand: true, src: ['index.html'], dest: 'dist/', filter: 'isFile'},
@@ -14,29 +21,37 @@ module.exports = function(grunt) {
         ]
       }
     },
+    
     concat: {
+      // concatenate js files (manually because of keeping the order intact)
       js: {
         src: ['vendor/jquery.js', 'vendor/modal.js', 'vendor/rAF.js', 'main.js'],
         dest: 'dist/main.js'
       },
+      // concatenate css files (manually because of keeping the order intact)
       css: {
         src: ['vendor/normalize.css', 'vendor/modal.css', 'vendor/loader.css', 'style.css'],
         dest: 'dist/style.css'        
       }
     },
+    
+    // minify the contents of the index.html file (in the `distributable` folder)
     htmlmin: {
       dist: {
         options: {
           removeComments: true,
           collapseWhitespace: true,
           removeAttributeQuotes: true,
-          removeRedundantAttributes: true
+          removeRedundantAttributes: true,
+          uglifyJS: true
         },
         files: {
           'dist/index.html': 'dist/index.html'
         }
       }
-    },    
+    },
+    
+    // minify the contents of the concatenated css file (in the `distributable` folder)
     cssmin: {
       combine: {
         files: {
@@ -44,12 +59,17 @@ module.exports = function(grunt) {
         }
       }
     },
+    
+    // minify the contents of the concatenated js file (in the `distributable` folder)
     uglify: {
       build: {
         src: 'dist/main.js',
         dest: 'dist/main.min.js'
       }
     },
+    
+    // revision the asset files names (in the `distributable` folder)
+    // patttern: ORIGINALFILENAME.{$HASH}.EXTENSION
     filerev: {
       options: {
         encoding: 'utf8',
@@ -66,10 +86,16 @@ module.exports = function(grunt) {
         src: 'dist/*.min.css'        
       },      
     },
+    
+    // replace original filenames with their revisioned coungterparts
+    // only changes in index.html & our minified css are necessary
     usemin: {
       html: 'dist/index.html',
       css: ['dist/*.min.*.css']
     },
+    
+    // apply lossless image compression
+    // does also copy the images over to the `distributable` folder 
     imagemin: {
       dynamic: {
         files: [{
@@ -80,6 +106,9 @@ module.exports = function(grunt) {
         }]
       }
     },
+    
+    // apply "lossless" svg optimization
+    // does also copy the svg´s over to the `distributable` folder    
     svgmin: {
       options: {
         plugins: [{
@@ -100,7 +129,8 @@ module.exports = function(grunt) {
           dest: 'dist/img'
         }]
       }
-    }       
+    }
+       
   });
 
   grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -115,5 +145,32 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-usemin');
     
   // Default task(s).
-  grunt.registerTask('default', ['clean:dist', 'concat', 'uglify', 'cssmin', 'copy', 'imagemin', 'svgmin', 'filerev', 'usemin', 'htmlmin', 'clean:afterMin']);
+  // ORDER IS IMPORTANT
+  grunt.registerTask('default', [
+    // clean the distributabel folder
+    'clean:dist',
+    // concatenate js/css files & copy them over to the dist folder
+    // must be done before uglify & cssmin
+    'concat',
+    // minify the concatenated js files in the dist folder
+    'uglify',
+    // minify the concatenated css files in the dist folder
+    'cssmin',
+    // copy over the static assets like the robots.txt
+    // static means here, that they won't be changed from some automated process
+    'copy',
+    // losslessy optimize images & copy them to the dist folder
+    'imagemin',
+    // losslessy optimize svgs & copy them to the dist folder 
+    'svgmin',
+    // revision all the assets in the dist folder (must be done after all assets are copied)
+    'filerev',
+    // exchange all the in file references with the revisioned counterparts (must be done after the filerev task)
+    'usemin',
+    // minify the html contents, must be done after the usemin task,
+    // because it relies on comments in the HTML source
+    'htmlmin',
+    // housekeeping; remove unecessary artifacts that were generated during the process
+    'clean:afterMin'
+  ]);
 };
