feb99bf15909bb5635a60f79ca2ed217c2e21d0a
Hans Christian Reinl <info@drublic.de>
2014-07-21 23:56:03 +0200

    Update hongkong.js to v0.2.0
    Ref. #21

--- vendor/hongkong.js
+++ vendor/hongkong.js
@@ -2,98 +2,65 @@
  * Parallax scrolling
  */
 (function ($) {
-	'use strict';
+    var $scrollTop = $('[data-parallax-top]');
+    var $scrollBottom = $('[data-parallax-bottom]');
+    var scrollPosition = 0;
 
-	var $scrollTop = $('[data-parallax-top]');
-	var $scrollBottom = $('[data-parallax-bottom]');
-	var factor;
-	var scrollPosition = 0;
+    /**
+     * Get the factor attribute for each
+     * @return {[type]} [description]
+     */
+    var _generateFactor = function () {
+        var i;
 
-	/**
-	 * Get the factor attribute for each
-	 * @return {[type]} [description]
-	 */
-	var _generateFactor = function () {
-		var i = 0;
-		var j = 0;
+        for (i = 0; i < $scrollTop.length; i++) {
+            $scrollTop[i].factor = parseInt($scrollTop[i].getAttribute('data-parallax-factor') || 4, 10);
+        }
 
-		for (; i < $scrollTop.length; i++) {
-			factor = $scrollTop[i].getAttribute('data-parallax-factor') || 4;
+        for (i = 0; i < $scrollBottom.length; i++) {
+            $scrollBottom[i].factor = parseInt($scrollBottom[i].getAttribute('data-parallax-factor') || 4, 10);
+        }
+    };
 
-			$scrollTop[i].factor = factor;
-		}
 
-		for (; j < $scrollBottom.length; j++) {
-			factor = $scrollBottom[j].getAttribute('data-parallax-factor') || 4;
+    /**
+     * Callback for rAF
+     * @return {void}
+     */
+    var _callback = function () {
+        var scroll = window.pageYOffset;
+        var i;
 
-			$scrollTop[j].factor = factor;
-		}
-	};
+        /**
+         * Eject if update isn't needed
+         */
+        if (scrollPosition === scroll) {
+            for (i = 0; i < $scrollTop.length; i++) {
+                $scrollTop[i].style.transform = 'translateY(0) translateZ(0)';
+            }
 
-	/**
-	 * Throttle scrolling
-	 * @param  {function} func
-	 * @param  {integer}  wait
-	 * @param  {boolean}  immediate
-	 * @return {function}
-	 */
-	var _throttle = function (func, wait, immediate) {
-		var timeout;
+            window.requestAnimationFrame(_callback);
 
-		return function () {
-			var later = function () {
-				timeout = null;
+            return false;
+        }
 
-				if (!immediate) {
-					func.apply(this, arguments);
-				}
-			};
+        for (i = 0; i < $scrollTop.length; i++) {
+            $scrollTop[i].style.transform = 'translateY(' + Math.floor(scroll / $scrollTop[i].factor) + 'px) translateZ(0)';
+        }
 
-			var callNow = immediate && !timeout;
+        for (i = 0; i < $scrollBottom.length; i++) {
+            $scrollBottom[i].style.transform = 'translateY(' + Math.floor(scroll / ($scrollBottom[i].factor * -1)) + 'px) translateZ(0)';
+        }
 
-			clearTimeout(timeout);
-			timeout = setTimeout(later, wait);
+        window.requestAnimationFrame(_callback);
+    };
 
-			if (callNow) {
-				func.apply(this, arguments);
-			}
-		};
-	};
+    /**
+     * Init
+     */
+    if ($scrollTop.length || $scrollBottom.length) {
+        _generateFactor();
+        window.requestAnimationFrame(_callback);
+    }
 
-	/**
-	 * Callback for throttle function
-	 * @return {void}
-	 */
-	var _callback = function () {
-		var scroll = window.pageYOffset;
-		var i;
-		var factor;
-
-		if (scrollPosition === scroll) {
-			window.cancelAnimationFrame(_callback);
-
-			return false;
-		}
-
-		for (i = 0; i < $scrollTop.length; i++) {
-			factor = $scrollTop[i].factor;
-			$scrollTop.eq(i).css('transform', 'translateY(' + parseInt(scroll / factor, 10) + 'px)');
-		}
-
-		for (i = 0; i < $scrollBottom.length; i++) {
-			factor = $scrollTop[i].factor;
-			$scrollTop.eq(i).css('transform', 'translateY(' + parseInt(scroll / (factor * -1), 10) + 'px)');
-		}
-
-		window.cancelAnimationFrame(_callback);
-	};
-
-	/**
-	 * Init
-	 */
-	 _generateFactor();
-
-	$(window).on('scroll', function () {
-		_throttle(window.requestAnimationFrame(_callback), 100, false);
-	});
 }(jQuery));
