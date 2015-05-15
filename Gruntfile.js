8c667ee563e3f01a33af05f43f3846303a454655
Sebastian Golasch <public@asciidisco.com>
2015-05-15 16:41:55 +0200

    fix(old-page): Add copy command to the grunt deploy task as well

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,6 +41,7 @@ module.exports = function(grunt) {
         files: [
           {expand: true, cwd: 'dist', src: ['img/**'], dest: '_site/'},
           {expand: true, cwd: 'dist', src: ['slides/**'], dest: '_site/'},
+          {expand: true, cwd: 'dist', src: ['2014/**/*'], dest: '_site/'},
           {expand: true, cwd: 'dist', src: ['*.html'], dest: '_site/'},
           {expand: true, cwd: 'dist', src: ['*.js'], dest: '_site/'},
           {expand: true, cwd: 'dist', src: ['*.css'], dest: '_site/'},
